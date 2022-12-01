e8708fe28c162fe68ee0e8e51d176319f55aad32
DesPetrikov <desik1603@gmail.com>
2022-12-01 22:04:20 +0300

    feat: fs/copy, delete, list, read

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -3,18 +3,21 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const copy = async () => {
+  try {
     const __dirname = path.dirname(fileURLToPath(import.meta.url));
     const srcDir = path.join(__dirname, 'files');
     const destDir = path.join(__dirname, 'files_copy');
-    try {
-        await cp(srcDir, destDir, {errorOnExist: true, force: false, recursive: true})
-    } catch (error) {
-        if (error.code === 'ENOENT' || error.code === 'ERR_FS_CP_EEXIST') {
-            throw new Error('FS operation failed')
-        }
-        throw error;
+    await cp(srcDir, destDir, {
+      errorOnExist: true,
+      force: false,
+      recursive: true,
+    });
+  } catch (error) {
+    if (error.code === 'ENOENT' || error.code === 'ERR_FS_CP_EEXIST') {
+      throw new Error('FS operation failed');
     }
-    
+    throw error;
+  }
 };
 
 copy();
