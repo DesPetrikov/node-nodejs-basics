57414e5aa03a09163111cdf836c0684e5ed8ad53
DesPetrikov <desik1603@gmail.com>
2022-12-01 19:28:47 +0300

    feat: fs/copy was implemented

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import { cp } from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const copy = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const srcDir = path.join(__dirname, 'files');
+    const destDir = path.join(__dirname, 'files_copy');
+    try {
+        await cp(srcDir, destDir, {errorOnExist: true, force: false, recursive: true})
+    } catch (error) {
+        if (error.code === 'ENOENT' || error.code === 'ERR_FS_CP_EEXIST') {
+            throw new Error('FS operation failed')
+        }
+        throw error;
+    }
+    
 };
 
 copy();
