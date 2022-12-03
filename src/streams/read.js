28c6ffabd92f9d1c5aa36a5d765fcf30a113af95
DesPetrikov <desik1603@gmail.com>
2022-12-03 22:13:08 +0300

    feat: streams were implemented

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import {stdout} from 'node:process';
+import { createReadStream } from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+    let content = ''
+    const stream = createReadStream(filePath, 'utf-8');
+    stream.on('data', chunk => content += chunk);
+    stream.on('end', () => stdout.write(content))
 };
 
 await read();
