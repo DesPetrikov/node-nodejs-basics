a925aa89fcf15ae1f0ad3a8efee811c95e16ebae
DesPetrikov <desik1603@gmail.com>
2022-12-04 22:10:28 +0300

    feat: zip methods was implemented

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream';
+import { promisify } from 'node:util';
+import { createGzip } from 'node:zlib';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const compress = async () => {
-    // Write your code here 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const srcPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const destPath = path.join(__dirname, 'files', 'archive.gz');
+  const gzip = createGzip();
+  const source = createReadStream(srcPath);
+  const destination = createWriteStream(destPath);
+  const pipe = promisify(pipeline);
+  await pipe(source, gzip, destination);
+  
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,29 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream';
+import { promisify } from 'node:util';
+import { createGunzip } from 'node:zlib';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const decompress = async () => {
-    // Write your code here 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const destPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const srcPath = path.join(__dirname, 'files', 'archive.gz');
+  try {
+    const gzip = createGunzip();
+    const source = createReadStream(srcPath);
+    const destination = createWriteStream(destPath);
+    const pipe = promisify(pipeline);
+    await pipe(source, gzip, destination);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.error(
+        `Error: ENOENT: no such file or directory, open ${srcPath}. Make sure that you have created this file with help of npm run zip:compress `
+      );
+    } else {
+      throw error;
+    }
+  }
 };
 
 await decompress();
